aa103d7e9f9f1cb7bac56bbbe819c61fa6e935ef
ClaudioLoGiudice7 <claudiologiudice6@gmail.com>
2023-06-14 11:07:02 +0200

    Entering milestone 3

---
+++ src/org/lessons/java/Concerto.java
@@ -0,0 +1,47 @@
+package org.lessons.java;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class Concerto extends Evento {
+    private LocalTime hour;
+    private BigDecimal price;
+
+    public Concerto(String title, LocalDate date, int totalSeats, LocalTime hour, BigDecimal price) throws Exception {
+        super(title, date, totalSeats);
+        this.hour = hour;
+        this.price = price;
+    }
+
+    public LocalTime getHour() {
+        return hour;
+    }
+
+    public void setHour(LocalTime hour) {
+        this.hour = hour;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getFormattedDateAndHour() {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        return getDate().format(dateTimeFormatter) + " " + hour.format(DateTimeFormatter.ofPattern("HH:mm"));
+    }
+
+    public String getFormattedPrice() {
+        return price.setScale(2).toString() + "€";
+    }
+
+    @Override
+    public String toString() {
+        return getFormattedDateAndHour() + " - " + getTitle() + " - " + getFormattedPrice();
+    }
+}
